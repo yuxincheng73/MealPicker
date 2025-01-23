cbb182671540a6455fceefad312181d257f07609
yuxincheng73 <yx.cheng@mail.utoronto.ca>
2025-01-23 14:41:48 -0500

    Initial push

---
+++ src/main/java/com/mealpicker/project/MealPickerApplication.java
@@ -0,0 +1,13 @@
+package com.mealpicker.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealPickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealPickerApplication.class, args);
+	}
+
+}
